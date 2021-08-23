05d392d67874dde625a7eaeda4b62ac42992386f
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-23 19:57:12 -0400

    created routes for participations, hosts and dashboards controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :tastings, only: [:index, :show, :create]
+  resources :tastings, only: [:index, :show, :create] do
+    resources :participations, only: :create
+  end
+  resources :participations, only: :update
+  resources :hosts, only: :show
+  resource :dashboard, only: :show
 end
