b156cb2117f3ce143e19e5afc63c05f5568b610e
Dylan Cherry <dylancherry98@gmail.com>
2021-08-24 14:44:24 -0400

    Dropped dashboards table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_182820) do
+ActiveRecord::Schema.define(version: 2021_08_24_184210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,11 +49,6 @@ ActiveRecord::Schema.define(version: 2021_08_24_182820) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_categorizations_on_category_id"
     t.index ["tasting_id"], name: "index_categorizations_on_tasting_id"
-  end
-
-  create_table "dashboards", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "participations", force: :cascade do |t|
