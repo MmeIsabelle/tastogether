d4eef08664c1c25b67b4a9358f4b201e94afde65
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-24 14:53:43 -0400

    rearranged order of code

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,9 +2,9 @@ class ParticipationsController < ApplicationController
   def create
     @tasting = Tasting.find(params[:tasting_id])
     @participation = Participation.new(participation_params)
-    authorize(@participation)
-    @participation.user = current_user
     @participation.tasting = @tasting
+    @participation.user = current_user
+    authorize(@participation)
     @participation.host = false
     @participation.status = "Pending"
     if @participation.save!
