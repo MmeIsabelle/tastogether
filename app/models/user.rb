a9e91258d8e273c46dd91e3fc72d81050a302c61
Dylan Cherry <dylancherry98@gmail.com>
2021-08-24 16:29:29 -0400

    Updated models' associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :participations
+  has_many :tastings, through: :participations
+
+  has_many :hosting_participations, -> { where(host: true, status: "accepted") }, class_name: "Participation"
+  has_many :hosted_tastings, through: :hosting_participations, source: :tasting
 end
