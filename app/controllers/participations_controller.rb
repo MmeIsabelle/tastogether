5cdb7f72f8228a9b4f7cfa9d4c263e7daa77af4f
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-24 14:29:22 -0400

    finished the create method in the participations controller and added the simple form to the tastings show page. Right now the authorization is set to true so that still needs to be worked out

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,7 +1,23 @@
 class ParticipationsController < ApplicationController
   def create
+    @tasting = Tasting.find(params[:tasting_id])
+    @participation = Participation.new(participation_params)
+    authorize(@participation)
+    @participation.user = current_user
+    @participation.tasting = @tasting
+    @participation.host = false
+    @participation.status = "Pending"
+    if @participation.save!
+      redirect_to "/"
+    else
+      render "/tastings/:tasting_id"
+    end
   end
 
   def update
+  end
+
+  def participation_params
+    params.require(:participation).permit(:initial_message)
   end
 end
--- app/policies/participation_policy.rb
+++ app/policies/participation_policy.rb
@@ -4,4 +4,8 @@ class ParticipationPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
 end
