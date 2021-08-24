5db27035bcee19c5a1e8f374a06420ed1725a40c
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-24 09:45:33 -0700

    updated tasting and category associations through categorizations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :categorizations
+  has_many :tastings, through: :categorizations
 end
--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -1,6 +1,7 @@
 class Tasting < ApplicationRecord
   belongs_to :user
   has_many :participations, dependent: :destroy
-  has_many :categories
+  has_many :categorizations
+  has_many :categories, through: :categorizations
   has_one_attached :photo
 end
