2713e65e2e2a8a791881b8fd30d2046a1279a1e4
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-25 12:52:31 -0400

    added basic validations to tasting model

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -6,5 +6,8 @@ class Tasting < ApplicationRecord
   has_one :host, through: :host_participation, source: :user
   has_one_attached :photo
 
-  validates :title, :description, :location, :date, presence: true
+  validates :title, :location, :date, presence: true
+  validates :title, uniqueness: true, length: { in: 10..75 }
+  validates :description, length: { maximum: 500 }
+  validates :capacity, numericality: { only_integer: true }
 end
