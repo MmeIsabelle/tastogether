cb73e9a3ed9be11e1336cbfbb200503cd7549276
Dylan Cherry <dylancherry98@gmail.com>
2021-08-25 16:44:03 -0400

    Added longitude and latitude to Users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_153829) do
+ActiveRecord::Schema.define(version: 2021_08_25_204217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2021_08_25_153829) do
     t.text "bio"
     t.string "avatar"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
