76d15a7e3349d7615787f847553eae9f7d0c6eae
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-25 14:52:10 -0400

    updated tasting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Destroy all
+puts "Emptying database"
 Participation.destroy_all
 Tasting.destroy_all
 User.destroy_all
 puts "All participations, tastings, and users deleted!"
+
+puts "Seeding database..."
+
+puts "Creating users..."
 
 User.create!(
   email: "user0@user.com",
@@ -40,7 +45,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user2@user.com",
+  email: "user3@user.com",
   password: "123456",
   address: "620 Saint-Catherine St W, Montreal, Quebec H3B 1B7, Canada",
   username: Faker::Internet.username,
@@ -49,7 +54,7 @@ User.create!(
 )
 
 User.create!(
-  email: "user2@user.com",
+  email: "user4@user.com",
   password: "123456",
   address: "192 Rue Saint-Zotique E, Montréal, QC H2S 1K8, Canada",
   username: Faker::Internet.username,
@@ -57,7 +62,7 @@ User.create!(
   avatar: Faker::Avatar.image
 )
 User.create!(
-  email: "user2@user.com",
+  email: "user5@user.com",
   password: "123456",
   address: "1478 Rue, Peel St, Montreal, Quebec H3A 1T1, Canada",
   username: Faker::Internet.username,
@@ -65,12 +70,14 @@ User.create!(
   avatar: Faker::Avatar.image
 )
 
-puts "You created #{User.count} Users."
+puts "Created #{User.count} Users."
+
+puts "Creating tastings and assigning users as hosts..."
 
 Tasting.create!(
   title: "Dark chocolate party",
   description: "Bring your favorite type of dark chocolate. The rarer the better.",
-  location: "555 fake avenue",
+  location: "620 Rue Cathcart #300, Montréal, QC H3B 1M1, Canada",
   date: Date.tomorrow,
   capacity: 5
 )
@@ -78,7 +85,7 @@ Tasting.create!(
 Tasting.create!(
   title: "Exotic coffee tasting",
   description: "Bring your coffee of choice. Coffee connaisseurs only.",
-  location: "888 test blvd",
+  location: "1255 Boulevard Robert-Bourassa Suite #1505, Montreal, Quebec H3B 3V8, Canada",
   date: Date.tomorrow + 1,
   capacity: 8
 )
@@ -86,9 +93,13 @@ Tasting.create!(
 Tasting.create!(
   title: "Pizza party",
   description: "We will all order pizza from our favorite pizza restaurants.",
-  location: "1234 Nadav street",
+  location: "5991 Victoria Ave, Montreal, Quebec H3W 2R9, Canada",
   date: Date.tomorrow + 10,
   capacity: 10
 )
+
+Tasting.all.each do |tasting|
+  tasting.host_participation = User.all.sample
+end
 
 puts "You created #{Tasting.count} Tastings."
