b7ca738872a7b21d59520083933f47463c2fbda3
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-26 08:32:11 -0700

    move the new tasting form to a separate page

--- app/controllers/tastings_controller.rb
+++ app/controllers/tastings_controller.rb
@@ -10,6 +10,11 @@ class TastingsController < ApplicationController
     authorize(@tasting)
   end
 
+  def new
+    @tasting = Tasting.new
+    authorize @tasting
+  end
+
   def create
     @tasting = Tasting.new(tasting_params)
     @tasting.host = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :tastings, only: [:index, :show, :create] do
+  resources :tastings, only: [:index, :show, :new, :create] do
     resources :participations, only: :create
   end
   resources :participations, only: :update
