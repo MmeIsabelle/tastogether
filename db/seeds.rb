e1a79728729f623c312b84529bb6f4c8a052df36
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-28 19:07:23 -0700

    added image link to seed and conditions to display in dashboard and tastings

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,58 +17,58 @@ users = {
   choco: User.create!(
     email: "user0@user.com",
     password: "123456",
-    address: "632 Rue Cathcart",
-    username: "Choco Nerd",
+    address: "632 Rue Cathcart, Montreal",
+    username: "Marc",
     bio: "Extreme chocolate lover. Lifelong music expert. Incurable gamer. Social media nerd. Food fan. Explorer.",
-    avatar: Faker::Avatar.image
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190849/marc.jpg'
   ),
   sweet: User.create!(
     email: "user1@user.com",
     password: "123456",
-    address: "8910 Rue Lajeunesse",
-    username: "All about sweets",
+    address: "8910 Rue Lajeunesse, Montreal",
+    username: "Nick",
     bio: "Student. Sweets nerd. Icecream-a-holic. Introvert. Evil music aficionado. Freelance pop culture advocate. Coffee trailblazer.",
-    avatar: Faker::Avatar.image
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190866/nick.jpg'
   ),
   wine: User.create!(
     email: "user2@user.com",
     password: "123456",
-    address: "1255 Phillips Square",
-    username: "Cheers to that",
-    bio: "Devoted alcohol enthusiast. Social media fan. Evil web geek. Introvert. Problem solver. Entrepreneur.",
-    avatar: Faker::Avatar.image
+    address: "1255 Phillips Square, Montreal",
+    username: "Arlette",
+    bio: "Wine specialist. Social media fan. Evil web geek. Introvert. Problem solver. Entrepreneur.",
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190836/arlette.jpg'
   ),
   coffee: User.create!(
     email: "user3@user.com",
     password: "123456",
-    address: "620 Saint-Catherine",
-    username: "Super Tastebuds",
+    address: "620 Saint-Catherine, Montreal",
+    username: "Corrine",
     bio: "Coffee evangelist. Wannabe travel trailblazer. Twitter nerd. Alcohol advocate. Social media aficionado. Pop culture ninja.",
-    avatar: Faker::Avatar.image
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190856/corrine.jpg'
   ),
   bobby: User.create!(
     email: "user4@user.com",
     password: "123456",
-    address: "192 Rue Saint-Zotique",
-    username: "Bobby M",
+    address: "192 Rue Saint-Zotique, Montreal",
+    username: "Bobby",
     bio: "Food expert. Travel geek. Pop culture scholar. Troublemaker. Gamer. Coffee guru. Internet evangelist. Writer. Beer buff.",
-    avatar: Faker::Avatar.image
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190853/bobby.jpg'
   ),
   edgar: User.create!(
     email: "user5@user.com",
     password: "123456",
-    address: "1478 Rue Peel St",
-    username: "Edgar",
-    bio: "Beer trailblazer. Italy lover. Twitter buff. Travel specialist. Avid alcohol maven.",
-    avatar: Faker::Avatar.image
+    address: "1478 Peel St, Montreal",
+    username: "Fabiola",
+    bio: "Beer trailblazer. Italy lover. Twitter buff. Travel specialist.",
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190839/Fabiola.jpg'
   ),
   meg: User.create!(
     email: "meg@user.com",
     password: "123456",
-    address: "5122 Av. des Érables",
-    username: "Flavours hunter",
+    address: "888 Wellington, Montreal",
+    username: "Meg",
     bio: "Food explorer. Web lover. Wine collector. Coffee enthusiast. Chocolate amateur.",
-    avatar: Faker::Avatar.image
+    avatar: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,b_rgb:262c35/v1630190929/meg-957fj3o9.jpg'
   )
 }
 
@@ -77,7 +77,7 @@ tastings = {
   dark: Tasting.create!(
     title: "Dark chocolate party",
     description: "A variety of dark chocolate from different origins will be shared to explore the nuances and the different notes. Bring your favorite! The rarer the better.",
-    location: "620 Rue Cathcart",
+    location: "620 Rue Cathcart, Montreal",
     date: DateTime.new(2021, 9, 3, 20, 0, 0),
     capacity: 5,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/v1629996108/Tastogether/dark_chocolate_k402i4.jpg'
@@ -85,7 +85,7 @@ tastings = {
   coffee_tasting: Tasting.create!(
     title: "Exotic coffee tasting",
     description: "Bring your coffee of choice. Coffee connaisseurs only.",
-    location: "1255 Boulevard Robert-Bourassa",
+    location: "620 Saint-Catherine, Montreal",
     start_at: DateTime.new(2021, 9, 6, 19, 0, 0),
     capacity: 4,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/a_90/v1629996106/Tastogether/coffee_lesk06.jpg'
@@ -93,7 +93,7 @@ tastings = {
   beans: Tasting.create!(
     title: "From the beans to the bar",
     description: "Chocolate made from scratch... Really! Let's taste chocolates made from beans from different countries. I will show you how I make it possible and we can discuss the subtle and not so subtle diffenrences in flavours.",
-    location: "1426 Rue Bishop",
+    location: "620 Rue Cathcart, Montreal",
     start_at: DateTime.new(2021, 9, 9, 19, 0, 0),
     capacity: 4,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/v1630000746/Tastogether/bean_to_bar_mzj7m2.jpg'
@@ -101,7 +101,7 @@ tastings = {
   gelato: Tasting.create!(
     title: "Gelato, just because...",
     description: "Gelato is just an amizing world to discover... Join in!",
-    location: "1290 Av. Bernard",
+    location: "8910 Rue Lajeunesse, Montreal",
     start_at: DateTime.new(2021, 9, 4, 15, 0, 0),
     capacity: 4,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/v1630000742/Tastogether/gelato_ela3ku.jpg'
@@ -109,7 +109,7 @@ tastings = {
   milk: Tasting.create!(
     title: "The milky way",
     description: "Because milk chocolates don't all taste the same! We will revisit some well know bars and compare them with some local chocolaters ",
-    location: "2221 Rue Workman",
+    location: "620 Rue Cathcart, Montreal",
     start_at: DateTime.new(2021, 9, 7, 20, 0, 0),
     capacity: 4,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/v1630000740/Tastogether/chocolate_bar_em9caz.jpg'
@@ -117,7 +117,7 @@ tastings = {
   vintage: Tasting.create!(
     title: "From old to new",
     description: "Let's taste the same wine from different vintages to compare and discuss the effect of aging on the flavours.",
-    location: "852 Rue Sherbrooke",
+    location: "1255 Phillips Square, Montreal",
     start_at: DateTime.new(2021, 9, 16, 19, 30, 0),
     capacity: 6,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/v1629996979/Tastogether/wine_bgbbom.jpg'
@@ -125,7 +125,7 @@ tastings = {
   pizza: Tasting.create!(
     title: "Pizza party",
     description: "We will all order pizza from our favorite pizza restaurants to compare their take on the margarita pizza.",
-    location: "5991 Victoria Ave",
+    location: "1478 Peel St, Montreal",
     start_at: DateTime.new(2021, 9, 11, 20, 0, 0),
     capacity: 10,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
