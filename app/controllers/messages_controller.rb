2cee4c6059e239d09fb78c046aae2ba2c536bcf8
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-30 15:37:18 -0700

    completed route and params for new message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,21 @@
 class MessagesController < ApplicationController
   def create
     @sender = current_user
-    @recipient = current_conversation.other_user.username
+    @recipient = User.find(params[:conversation_other_user_id])
     @message = Message.new(message_params)
+    @message.sender = @sender
+    @message.recipient = @recipient
     if @message.save
-      redirect_to conversations_path
+      redirect_to conversations_path(anchor: "message-#{@message.id}")
     else
-      #TODO
+      render conversations_path
     end
+    authorize @message
   end
 
   private
 
   def message_params
-    require.params(:message).permit(:content)
+    params.require(:message).permit(:content)
   end
 end
