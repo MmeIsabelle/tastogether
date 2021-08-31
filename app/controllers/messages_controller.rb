27a3015c932e649099da9c576a307e7384ecfa8c
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-30 21:18:46 -0700

    added validation and remove remote to get the error when not saved

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,17 @@
 class MessagesController < ApplicationController
   def create
+    @conversations = policy_scope(Conversation)
     @sender = current_user
     @recipient = User.find(params[:conversation_other_user_id])
+    @current_conversation = current_conversation
+  
     @message = Message.new(message_params)
     @message.sender = @sender
     @message.recipient = @recipient
     if @message.save
       redirect_to conversations_path(anchor: "message-#{@message.id}")
     else
-      render conversations_path
+      render 'conversations/index'
     end
     authorize @message
   end
@@ -17,5 +20,12 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content)
+  end
+
+  def current_conversation
+    Conversation.new(
+      main_user: current_user,
+      other_user: User.find(params[:conversation_other_user_id])
+    )
   end
 end
