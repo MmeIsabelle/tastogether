ab39a5dfef3911e0149be497dee86c90edb575c5
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-09-01 13:51:21 -0400

    added tastings, categories, categorizations plus refactored code to make it easier to look through'

--- db/seeds.rb
+++ db/seeds.rb
@@ -102,7 +102,7 @@ tastings = {
   ),
   gelato: Tasting.create!(
     title: "Gelato, just because...",
-    description: "Gelato is just an amizing world to discover... Join in!",
+    description: "Gelato is just an amazing world to discover... Join in!",
     location: "8910 Rue Lajeunesse, Montreal",
     start_at: DateTime.new(2021, 9, 4, 15, 0, 0),
     capacity: 4,
@@ -132,6 +132,9 @@ tastings = {
     capacity: 10,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
   ),
+
+  # All below tastings need new cloudinary images. Will now just be reusing the cloudinary pizza image for the pizza party tasting
+
   barbeque: Tasting.create!(
     title: "Barbeque Bash",
     description: "We will barbeque up all kinds of meats.",
@@ -139,152 +142,262 @@ tastings = {
     start_at: DateTime.new(2021, 8, 29, 20, 0, 0),
     capacity: 10,
     image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  banh_mi: Tasting.create!(
+    title: "Banh Mi: Best Sandwich",
+    description: "In my opinion a banh mi is the best kind of sandwich in the world. Duh. All who share my opinon are welcome to come over and bring their own banh mi sandwich favorites for others to delight in. I will personally have several different varieties of banh mi sandwiches for us to eat. It's gonna be a great time!",
+    location: "1255 rue Jeanne-Mance",
+    start_at: DateTime.new(2021, 9, 9, 12, 30, 0),
+    capacity: 7,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  tea: Tasting.create!(
+    title: "Tea and Biscuits Time",
+    description: "Growing up in England I loved chatting and catching up with my friends while drinking tea. It is one of the things I miss the most about the U.K. now that I live in North America. So, I am looking for at most 4 other people to come over and chat over a nice cup of tea. I have a large selection for us to sample.",
+    location: "900 Blvd. Rene Levesque West, Montréal",
+    start_at: DateTime.new(2021, 9, 8, 2, 30, 0),
+    capacity: 4,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  beer: Tasting.create!(
+    title: "BYOB IPA Style",
+    description: "I am a huge fan of IPA's and recently I have found that there are so many more under the radar brands. Whether you are a fan of bitter or sweet IPA's, bring your favourite IPA and we will do a sampling of all the IPA's that everyone brings and debate which is the best! Please register quickly as I expect the available spots to be taken quickly. Food will be provided. ",
+    location: "80 Sherbrooke Street West",
+    start_at: DateTime.new(2021, 10, 3, 4, 30, 0),
+    capacity: 25,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  spice: Tasting.create!(
+    title: "Spice is Life",
+    description: "Are you a fan of the show Hot Ones? I am hosting a hot sauce challenge where each of us will eat 10 hot sauces on chicken wings barbequed by me. Each round the hot sauce will hit harder and harder and any who are left standing will be crowned champions and will most likely burn all their taste buds. I am hoping for a huge turnout, so if you like spicey food and pushing yourself to the limit, then be there.",
+    location: "5 Samson Blvd, Laval",
+    start_at: DateTime.new(2021, 9, 27, 6, 20, 0),
+    capacity: 50,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  italian: Tasting.create!(
+    title: "Italian Food Lovers",
+    description: "Calling all Italian food lovers. Me and my friends cannot get enough of Italian food and are looking to try out and experiment with new recipes. We are hosting an event where attendees can bring their favourite Italian recipes and we can all sample them and get each other's recipes! There will be 8 spots available. I will provide white wine for all of us as well.",
+    location: "10 Sherbrooke Ouest, Montréal",
+    start_at: DateTime.new(2021, 9, 16, 6, 0, 0),
+    capacity: 8,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  fried: Tasting.create!(
+    title: "Fried Food Fest",
+    description: "Hey everyone! Who doesn't love fried food? Whether it's for a meal like fried chicken and curly fries or for desert like fried ice cream , fried food is just awesome. I welcome all fried food lovers to come and enjoy the all the fried food they can ever eat. No need to bring anything. Everything will be provided. See you there.",
+    location: "421 Rue St. Vincent, Montréal",
+    start_at: DateTime.new(2021, 9, 22, 7, 0, 0),
+    capacity: 25,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
+  ),
+  taco: Tasting.create!(
+    title: "Taco Takeover",
+    description: "For all you taco people out there, this event is for you. Me and my wife love making all different kinds of authentic tacos and sharing them with others. So, bring a bottle of wine and let's meet up over some Tacos in my backyard. As of now we have 8 spots available. Note that me and my wife are in our late thirties, so we would prefer if attendees are around that age.",
+    location: "360 rue St-Antoine Ouest, Montréal",
+    start_at: DateTime.new(2021, 9, 18, 7, 30, 0),
+    capacity: 8,
+    image: 'https://res.cloudinary.com/dd3n6uf2t/image/upload/w_500,ar_16:9,c_fill,g_auto,e_sharpen/v1629996975/Tastogether/pizza_ixu3hd.jpg'
   )
 }
 
-#Creating hosts
-Participation.create!(
-  tasting: tastings[:dark],
-  user: users[:choco],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
+participations = {
+  #Creating hosts
+  host: {
+    host_dark: Participation.create!(
+      tasting: tastings[:dark],
+      user: users[:choco],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_coffee: Participation.create!(
+      tasting: tastings[:coffee_tasting],
+      user: users[:coffee],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_beans: Participation.create!(
+      tasting: tastings[:beans],
+      user: users[:choco],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_gelato: Participation.create!(
+      tasting: tastings[:gelato],
+      user: users[:sweet],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_milk: Participation.create!(
+      tasting: tastings[:milk],
+      user: users[:choco],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_vintage: Participation.create!(
+      tasting: tastings[:vintage],
+      user: users[:wine],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_pizza: Participation.create!(
+      tasting: tastings[:pizza],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_barbeque: Participation.create!(
+      tasting: tastings[:barbeque],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_banh: Participation.create!(
+      tasting: tastings[:banh_mi],
+      user: users[:bobby],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_tea: Participation.create!(
+      tasting: tastings[:tea],
+      user: users[:wine],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_beer: Participation.create!(
+      tasting: tastings[:beer],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_spice: Participation.create!(
+      tasting: tastings[:spice],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_italian: Participation.create!(
+      tasting: tastings[:italian],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_fried: Participation.create!(
+      tasting: tastings[:fried],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    ),
+    host_taco: Participation.create!(
+      tasting: tastings[:taco],
+      user: users[:edgar],
+      host: true,
+      initial_message: Faker::Lorem.sentence,
+      status: "accepted"
+    )
+  },
+  # Creating participations
+  not_hosted: {
+    beans: [
+      Participation.create!(
+        tasting: tastings[:beans],
+        user: users[:bobby],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "accepted"
+      ),
+      Participation.create!(
+        tasting: tastings[:beans],
+        user: users[:edgar],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      ),
+        Participation.create!(
+          tasting: tastings[:beans],
+          user: users[:wine],
+          host: false,
+          initial_message: Faker::Lorem.sentence,
+          status: "pending"
+      ),
+        Participation.create!(
+          tasting: tastings[:beans],
+          user: users[:meg],
+          host: false,
+          initial_message: Faker::Lorem.sentence,
+          status: "accepted"
+      )],
+    vintage: [
+      Participation.create!(
+        tasting: tastings[:vintage],
+        user: users[:sweet],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      ),
+      Participation.create!(
+        tasting: tastings[:vintage],
+        user: users[:coffee],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "accepted"
+      ),
+      Participation.create!(
+        tasting: tastings[:vintage],
+        user: users[:bobby],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      )],
+    milk: [
+      Participation.create!(
+        tasting: tastings[:milk],
+        user: users[:sweet],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      )],
+    gelato: [
+      Participation.create!(
+        tasting: tastings[:gelato],
+        user: users[:wine],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      )],
+    bbq: [
+      Participation.create!(
+        tasting: tastings[:barbeque],
+        user: users[:meg],
+        host: false,
+        initial_message: Faker::Lorem.sentence,
+        status: "pending"
+      )]
+    }
+}
 
-Participation.create!(
-  tasting: tastings[:coffee_tasting],
-  user: users[:coffee],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:beans],
-  user: users[:choco],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:gelato],
-  user: users[:sweet],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:milk],
-  user: users[:choco],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:vintage],
-  user: users[:wine],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:pizza],
-  user: users[:edgar],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
-Participation.create!(
-  tasting: tastings[:barbeque],
-  user: users[:edgar],
-  host: true,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-# Creating participations
-Participation.create!(
-  tasting: tastings[:beans],
-  user: users[:bobby],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
 
-Participation.create!(
-  tasting: tastings[:beans],
-  user: users[:edgar],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
 
-Participation.create!(
-  tasting: tastings[:vintage],
-  user: users[:sweet],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
-
-Participation.create!(
-  tasting: tastings[:vintage],
-  user: users[:coffee],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
-
-Participation.create!(
-  tasting: tastings[:vintage],
-  user: users[:bobby],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
-
-Participation.create!(
-  tasting: tastings[:beans],
-  user: users[:wine],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
-
-Participation.create!(
-  tasting: tastings[:beans],
-  user: users[:meg],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "accepted"
-)
-
-Participation.create!(
-  tasting: tastings[:milk],
-  user: users[:sweet],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
-
-Participation.create!(
-  tasting: tastings[:gelato],
-  user: users[:wine],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
-Participation.create!(
-  tasting: tastings[:barbeque],
-  user: users[:meg],
-  host: false,
-  initial_message: Faker::Lorem.sentence,
-  status: "pending"
-)
 
 # Creating categories
 categories = {
@@ -293,40 +406,168 @@ categories = {
   coffee: Category.create!(name: "Coffee"),
   beer: Category.create!(name: "Beer"),
   wine: Category.create!(name: "Wine"),
+  pasta: Category.create!(name: "Pasta"),
+  sandwich: Category.create!(name: "Sandwich"),
+  vietnamese: Category.create!(name: "Vietnamese"),
+  italian: Category.create!(name: "Italian"),
+  japanese: Category.create!(name: "Japanese"),
+  chinese: Category.create!(name: "Chinese"),
+  salad: Category.create!(name: "Salad"),
   gelato: Category.create!(name: "Gelato"),
+  healthy: Category.create!(name: "Healthy"),
+  sweet: Category.create!(name: "Sweet"),
+  sour: Category.create!(name: "Sour"),
+  spice: Category.create!(name: "Spicy"),
+  salty: Category.create!(name: "Salty"),
+  fish: Category.create!(name: "Fish"),
+  taco: Category.create!(name: "Taco"),
+  fried: Category.create!(name: "Fried"),
+  chicken: Category.create!(name: "Chicken"),
+  Vegetarian: Category.create!(name: "Vegetarian"),
+  vegan: Category.create!(name: "Vegan"),
+  tea: Category.create!(name: "Tea"),
+  barbeque: Category.create!(name: "Barbeque"),
+  ipa: Category.create!(name: "IPA")
 }
 
 # Creating Categorizations
-Categorization.create!(
-  category: categories[:pizza],
-  tasting: tastings[:pizza]
-)
+categorizations_for_tastings = {
+  milk: [
+    Categorization.create!(
+      category: categories[:chocolate],
+      tasting: tastings[:milk]
+    ),
+    Categorization.create!(
+      category: categories[:sweet],
+      tasting: tastings[:milk]
+    )
+  ],
+  beans: [
+    Categorization.create!(
+      category: categories[:chocolate],
+      tasting: tastings[:beans]
+    ),
+    Categorization.create!(
+      category: categories[:sweet],
+      tasting: tastings[:beans]
+    )
+  ],
+  coffee_tasting: [
+    Categorization.create!(
+      category: categories[:coffee],
+      tasting: tastings[:coffee_tasting]
+    )
+  ],
+  dark: [
+    Categorization.create!(
+      category: categories[:chocolate],
+      tasting: tastings[:dark]
+    ),
+    Categorization.create!(
+      category: categories[:sweet],
+      tasting: tastings[:dark]
+    )
+  ],
+  gelato: [
+    Categorization.create!(
+      category: categories[:gelato],
+      tasting: tastings[:gelato]
+    ),
+    Categorization.create!(
+      category: categories[:sweet],
+      tasting: tastings[:gelato]
+    )
+  ],
+  vintage: [
+    Categorization.create!(
+      category: categories[:wine],
+      tasting: tastings[:vintage]
+    )
+  ],
+  pizza: [
+    Categorization.create!(
+      category: categories[:pizza],
+      tasting: tastings[:pizza]
+    )
+  ],
+  bbq: [
+    Categorization.create!(
+      category: categories[:barbeque],
+      tasting: tastings[:barbeque]
+    )
+  ],
+  banh: [
+    Categorization.create!(
+      category: categories[:sandwich],
+      tasting: tastings[:banh_mi]
+    ),
+    Categorization.create!(
+      category: categories[:vietnamese],
+      tasting: tastings[:banh_mi]
+    )
+  ],
+  tea: [
+    Categorization.create!(
+      category: categories[:tea],
+      tasting: tastings[:tea]
+    )
+  ],
+  beer: [
+    Categorization.create!(
+      category: categories[:beer],
+      tasting: tastings[:beer]
+    ),
+    Categorization.create!(
+      category: categories[:ipa],
+      tasting: tastings[:beer]
+    )
+  ],
+  spice: [
+    Categorization.create!(
+      category: categories[:spice],
+      tasting: tastings[:spice]
+    ),
+    Categorization.create!(
+      category: categories[:spice],
+      tasting: tastings[:spice]
+    )
+  ],
+  italian: [
+    Categorization.create!(
+      category: categories[:pasta],
+      tasting: tastings[:italian]
+    ),
+    Categorization.create!(
+      category: categories[:pizza],
+      tasting: tastings[:italian]
+    ),
+    Categorization.create!(
+      category: categories[:italian],
+      tasting: tastings[:italian]
+    )
+  ],
+  fried: [
+    Categorization.create!(
+      category: categories[:fried],
+      tasting: tastings[:fried]
+    ),
+    Categorization.create!(
+      category: categories[:barbeque],
+      tasting: tastings[:fried]
+    )
+  ],
+  taco: [
+    Categorization.create!(
+      category: categories[:taco],
+      tasting: tastings[:taco]
+    ),
+    Categorization.create!(
+      category: categories[:healthy],
+      tasting: tastings[:taco]
+    )
+  ]
+}
 
-Categorization.create!(
-  category: categories[:chocolate],
-  tasting: tastings[:milk]
-)
 
-Categorization.create!(
-  category: categories[:chocolate],
-  tasting: tastings[:beans]
-)
-
-Categorization.create!(
-  category: categories[:coffee],
-  tasting: tastings[:coffee_tasting]
-)
-Categorization.create!(
-  category: categories[:chocolate],
-  tasting: tastings[:dark]
-)
-Categorization.create!(
-  category: categories[:gelato],
-  tasting: tastings[:gelato]
-)
-Categorization.create!(
-  category: categories[:wine],
-  tasting: tastings[:vintage]
-)
 
 puts "All done!"
