6376e603464a5eb0198092d52ab54885a5930362
Caio Andrade <andrade@hey.com>
2021-09-02 08:43:12 -0500

    Move user id to navbar element

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -1,11 +1,14 @@
 import consumer from "./consumer";
 
 const initNotificationCable = () => {
-  const notificationBadge = document.getElementById('notification');
-  if (notificationBadge && !window.notificationBadgeConnection) {
-    const id = notificationBadge.dataset.userId;
+  const navbarElement = document.getElementById('navbar');
+  const messageBadge = document.getElementById('message-notification-badge');
+  const requestBadge = document.getElementById('request-notification-badge');
+  const userId = navbarElement.dataset.userId;
+  if (userId && !window.notificationBadgeConnection) {
+    debugger
 
-    consumer.subscriptions.create({ channel: "NotificationChannel", id: id }, {
+    consumer.subscriptions.create({ channel: "NotificationChannel", id: userId }, {
       received(data) {
         console.log(data); // called when data is broadcast in the cable
         document.body.insertAdjacentHTML("beforeend", data.template)
@@ -13,11 +16,11 @@ const initNotificationCable = () => {
         notificationBadge.parentElement.classList.remove('d-none')
       },
       connected(data) {
-        console.log('connected', id);
+        console.log('connected', userId);
         window.notificationBadgeConnection = true
       },
       disconnected(data) {
-        console.log('connected', id);
+        console.log('connected', userId);
         window.notificationBadgeConnection = false
       }
     });
